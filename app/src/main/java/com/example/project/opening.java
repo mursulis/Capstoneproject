6e4fb9ca579aa8ad6fc3b22e83346357de3f7437
velazqga18 <55995769+velazqga18@users.noreply.github.com>
2020-10-29 19:13:12 -0700

    Initial commit

---
+++ app/src/main/java/com/example/project/opening.java
@@ -0,0 +1,34 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class opening extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opensecondpage();
+
+            }
+        });
+
+    }
+    public void opensecondpage(){
+        Intent intent = new Intent(this, secondpage.class);
+        startActivity(intent);
+
+    }
+
+}
